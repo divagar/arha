414403df4f9a6f477afb7e7718e2a286d7c5a479
Div <m.divagar@gmail.com>
2017-05-07 23:15:38 +0530

    Initial Commit.
    Signed-off-by: Div <m.divagar@gmail.com>

---
+++ src/app/arha.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './arha.component.html',
+  styleUrls: ['./arha.component.css']
+})
+export class ArhaComponent {
+  title = 'Arha... !';
+}
--- src/app/arha.module.ts
+++ src/app/arha.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { ArhaComponent } from './arha.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ArhaComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ArhaComponent]
 })
 export class AppModule { }
