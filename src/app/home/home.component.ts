30b4da6f44baad21bb772d5084b2d26e64f64b2e
Div <m.divagar@gmail.com>
2017-05-25 23:39:37 +0530

    Firebase Auth - provider Google sign up
    Signed-off-by: Div <m.divagar@gmail.com>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  user: Observable<firebase.User>;
+
+  constructor(public afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    console.log(this.user);
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
