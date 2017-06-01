846620f2d149ab7e627f0ffe606b2f9a4fc71852
Div <m.divagar@gmail.com>
2017-06-02 00:30:22 +0530

    arha: login guard added to tips page.
    Signed-off-by: Div <m.divagar@gmail.com>

--- src/app/tips/tips.component.ts
+++ src/app/tips/tips.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes, Router } from '@angular/router';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+import { ArhaauthService } from '../providers/arhaauth.service';
 
 @Component({
   selector: 'arha-tips',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TipsComponent implements OnInit {
 
-  constructor() { }
+  user: Observable<firebase.User>;
+
+  constructor(public authService: ArhaauthService,
+    public router: Router) {
+    this.user = authService.getUserDetails();
+    //get af auth status
+    this.user
+      .do(user => this.checkLogin(user))
+      .subscribe(user => this.checkLogin(user))
+  }
 
   ngOnInit() {
+  }
+
+  checkLogin(user) {
+    if (user == null)
+      this.router.navigate(['/']);
   }
 
 }
