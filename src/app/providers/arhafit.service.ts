0fdc6b12c9918ffc3264ff97dc68730d70c68209
Div <m.divagar@gmail.com>
2017-06-18 23:55:00 +0530

    arha: fit service observable style sample.
    Signed-off-by: Div <m.divagar@gmail.com>

--- src/app/providers/arhafit.service.ts
+++ src/app/providers/arhafit.service.ts
@@ -1,10 +1,45 @@
-import { Injectable } from '@angular/core';
+
+import { Injectable }              from '@angular/core';
+import { Http, Response }          from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ArhaFitService {
 
-  constructor() {
+  private gFitUrl = 'https://www.googleapis.com/fitness/v1/users/me/';
+  private gFitResponse = {};
+
+  constructor (private http: Http) {}
+
+  getDataSource(): Observable<any> {
+    let dataSourceUrl = 'dataSources'
+    console.log('I am here @ getDataSource');
+    return this.http.get(this.gFitUrl + dataSourceUrl)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    console.log(body);
+    return body.data || { };
+  }
+
+  private handleError (error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 
 }
